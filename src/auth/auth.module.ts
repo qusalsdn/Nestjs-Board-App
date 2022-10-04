2453524d60dc5ded3ce684391081ae6fbe9fc884
MinWooByun <yja05092@gmail.com>
2022-10-04 16:52:40 +0900

    jwt를 이용해서 토큰 생성
    필요한 모듈 설치 명령어: npm install @nestjs/jwt @nestjs/passport passport passport-jwt --save

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRespository } from './user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRespository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRespository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
